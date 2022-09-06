b7d4035f917f203dddb6a80b1d1a47c20abd7983
Sunset-kim <wood2002@naver.com>
2022-09-07 08:36:30 +0900

    tast: load contents and render  when user click list

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const ajax = new XMLHttpRequest();
-const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const BASE_URL = "https://api.hnpwa.com/v0";
+const NEWS_URL = `${BASE_URL}/news/1.json`;
+const ITEMS_URL = (id) => `${BASE_URL}/item/${id}.json`;
 
 ajax.open("get", NEWS_URL, false);
 ajax.send();
 
 const newFeeds = JSON.parse(ajax.response);
+
+const app = document.getElementById("app");
+const container = document.createElement("div");
 const ul = document.createElement("ul");
+
+window.addEventListener("hashchange", () => {
+  const id = window.location.hash.substring(1);
+  ajax.open("get", ITEMS_URL(id), false);
+  ajax.send();
+  const content = JSON.parse(ajax.response);
+
+  console.log(content);
+
+  const h2 = document.createElement("h2");
+  h2.innerText = content.title;
+
+  container.appendChild(h2);
+});
 
 const contents = newFeeds.forEach((feed) => {
   const li = document.createElement("li");
-  li.innerHTML = feed.title;
+  const a = document.createElement("a");
+
+  a.innerHTML = `${feed.title} (${feed.comments_count})`;
+  a.setAttribute("href", `#${feed.id}`);
+  li.appendChild(a);
   ul.appendChild(li);
 });
 
-document.getElementById("app").appendChild(ul);
+app.appendChild(ul);
+app.appendChild(container);
