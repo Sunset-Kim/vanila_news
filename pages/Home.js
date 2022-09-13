1eced0fa551737b6405ee7782d2ca1e8947affe6
Sunset-kim <wood2002@naver.com>
2022-09-13 23:08:12 +0900

    style: tailwind css

--- pages/Home.js
+++ pages/Home.js
@@ -11,21 +11,39 @@ export default async function Home(page) {
   });
 
   try {
-    const newsfeeds = await hackerAPI.getNewsfeeds();
-
-    store.setState({
-      feeds: newsfeeds,
-    });
+    if (!store.state.feeds) {
+      const newsfeeds = await hackerAPI.getNewsfeeds();
+      store.setState({
+        feeds: newsfeeds,
+      });
+    }
 
     const totalLength = store.state.feeds.length;
-
-    const result = [];
-    result.push("<ul>");
+    const newsList = [];
+    let template = `
+    <div class="min-h-screen bg-gray-50">
+      
+        <div class="bg-gray-800 mx-auto px-2 sm:px-6 lg:px-8 xl:px-10">
+          <h1 class="text-white font-bold">Hacker News</h1>
+        </div>
+        
+        <div class="mx-auto px-2 sm:px-6 lg:px-8 xl:px-10">
+          <ul>
+            {{__news_feed__}}
+          </ui>
+          <div>
+            <a href="#/page/{{__prev_page__}}">이전페이지</a>
+            <a href="#/page/{{__next_page__}}">다음페이지</a>
+          </div>
+        </div>
+      
+      
+    </div>`;
 
     const feeds = store.state.feeds;
 
     for (let i = (currentPage - 1) * 5; i < (currentPage - 1) * 5 + 5; i++) {
-      result.push(`
+      newsList.push(`
       <li>
         <a href="#/news/${feeds[i].id}">
           ${feeds[i].title} (${feeds[i].comments_count})
@@ -34,16 +52,14 @@ export default async function Home(page) {
       `);
     }
 
-    result.push("</ul>");
+    template = template.replace("{{__news_feed__}}", newsList.join(""));
+    template = template.replace("{{__prev_page__}}", store.state.currentPage > 1 ? store.state.currentPage - 1 : 1);
+    template = template.replace(
+      "{{__next_page__}}",
+      store.state.currentPage >= totalLength / 5 ? totalLength / 5 : store.state.currentPage + 1
+    );
 
-    result.push(`
-    <div>
-      <a href="#/page/${currentPage - 1 < 1 ? 1 : currentPage - 1}">이전페이지</a>
-      <a href="#/page/${currentPage + 1 > totalLength / 5 ? totalLength / 5 : currentPage + 1}">다음페이지</a>
-    </div>
-    `);
-
-    return result.join(" ");
+    return template;
   } catch (error) {
     console.log("navigate 500");
   }
